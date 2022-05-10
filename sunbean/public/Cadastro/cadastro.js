5084ce91e1cbf0f040625aa7461259d7b3a47154
Pedr0Henriqu <pedro.mendonca@sptech.school>
2022-05-09 21:06:12 -0300

    Adicionando api no site

---
+++ sunbean/public/Cadastro/cadastro.js
@@ -0,0 +1,130 @@
+function validCadastro() {
+  var nomeVar = input_nome_razao.value;
+  var telVar = input_tel.value;
+  var senhaVar = input_senha.value;
+  var cpfVar = input_cpf_cnpj.value;
+  var emailVar = input_email.value;
+
+  /* Verifica se há algum input com valor vazio e ajusta o layout para o texto caber na tela caso houver */
+  // if (input_nome_razao.value == '' || input_tel.value == '' || input_senha.value == '' || input_cpf_cnpj.value == '' || input_email.value == '' || input_repetir_senha.value == '') {
+  //   span_validacao.innerHTML = 'Por favor preencha todos os campos';
+  //   idcontainer2.style.marginTop = "-25px";
+  //   texto_cadastro.style.marginBottom = "30px"
+
+  //   /* marca o campo cpf/cnpj se estiver vazio */
+  //   if (input_cpf_cnpj.value !== '') {
+  //     input_cpf_cnpj.style.border = "none"
+  //   } else {
+  //     input_cpf_cnpj.style.border = "thin solid #FF0000"
+  //   }
+
+  //   /* marca o campo email se estiver vazio */
+  //   if (input_email.value !== '') {
+  //     input_email.style.border = "none"
+  //   } else {
+  //     input_email.style.border = "thin solid #FF0000"
+  //   }
+
+  //   /* marca o campo nome/razão social se estiver vazio */
+  //   if (input_nome_razao.value !== '') {
+  //     input_nome_razao.style.border = "none"
+  //   } else {
+  //     input_nome_razao.style.border = "thin solid #FF0000"
+  //   }
+
+  //   /* marca o campo senha se estiver vazio */
+  //   if (input_senha.value !== '') {
+  //     input_senha.style.border = "none"
+  //   } else {
+  //     input_senha.style.border = "thin solid #FF0000"
+  //   }
+
+  //   /* marca o campo repetir senha se estiver vazio */
+  //   if (input_repetir_senha.value !== '') {
+  //     input_repetir_senha.style.border = "none"
+  //   } else {
+  //     input_repetir_senha.style.border = "thin solid #FF0000"
+  //   }
+
+  //   /* marca o campo telefone se estiver vazio */
+  //   if (input_tel.value !== '') {
+  //     input_tel.style.border = "none"
+  //   } else {
+  //     input_tel.style.border = "thin solid #FF0000"
+  //   }
+  // }
+
+  // else {
+  //   /* tira as marcações das inputs já preenchidas */
+  //   input_tel.style.border = "none"
+  //   input_senha.style.border = "none"
+  //   input_repetir_senha.style.border = "none"
+  //   input_nome_razao.style.border = "none"
+  //   input_email.style.border = "none"
+  //   input_cpf_cnpj.style.border = "none"
+
+  //   /* Valida se o email possui "@" */
+  //   if (input_email.value.indexOf("@") == -1) {
+  //     input_email.style.border = "thin solid #FF0000"
+  //     span_validacao.innerHTML = 'Email inválido, deve conter "@"'
+  //   }
+
+  //   /* Valida se o email possui mais de 10 caracteres */
+  //   else if (input_email.value.length < 10) {
+  //     input_email.style.border = "thin solid #FF0000"
+  //     span_validacao.innerHTML = 'Email inválido, deve conter no mínimo 10 digitos'
+  //   }
+
+  //   /* Valida se o nome possui mais de 6 caracteres */
+  //   else if (input_nome_razao.value.length < 6) {
+  //     input_nome_razao.style.border = "thin solid #FF0000"
+  //     span_validacao.innerHTML = 'Nome deve conter no mínimo 6 digitos'
+  //   }
+
+  //   /* Valida se a input cpf/cnpj possui 11 ou 14 caracteres */
+  //   else if (input_cpf_cnpj.value.length !== 11 && input_cpf_cnpj.value.length !== 14) {
+  //     input_cpf_cnpj.style.border = "thin solid #FF0000"
+  //     span_validacao.innerHTML = 'Valor inválido, digite um valor de 11 (cpf) ou 14 (cnpj) numeros'
+  //   }
+
+  //   /* Valida se o número de telefone possui ao menos 8 digitos */
+  //   else if (input_tel.value.length < 8) {
+  //     input_tel.style.border = "thin solid #FF0000"
+  //     span_validacao.innerHTML = 'Telefone deve conter no mínimo 8 digitos'
+  //   }
+
+  //   /* Valida se a senha tem mais de 8 caracteres */
+  //   else if (input_senha.value.length < 8) {
+  //     input_senha.style.border = "thin solid #FF0000"
+  //     span_validacao.innerHTML = 'Senha deve conter no mínimo 8 digitos'
+  //   }
+
+  //   /* Valida se as inputs de senha e confirmar senha são iguais */
+  //   else if (input_senha.value !== input_repetir_senha.value) {
+  //     input_senha.style.border = "thin solid #FF0000"
+  //     input_repetir_senha.style.border = "thin solid #FF0000"
+  //     span_validacao.innerHTML = 'As senhas não coincidem'
+  //   }
+
+  //   /* Valida se o checkbox de termos de uso foi aceito */
+  //   else if (chk_termos.checked == false) {
+  //     span_validacao.innerHTML = 'Você deve concordar com os nossos termos de uso antes de prosseguir'
+  //     chk_termos.style.outline = "1px solid red"
+  //     chk_termos.style.outlineOffset = "-1px"
+  //   }
+
+  //   /* Confirma o cadastro e abre a tela de monitoramento */
+  //   else {
+  //     input_tel.style.border = "none"
+  //     input_senha.style.border = "none"
+  //     input_repetir_senha.style.border = "none"
+  //     input_nome_razao.style.border = "none"
+  //     input_email.style.border = "none"
+  //     input_cpf_cnpj.style.border = "none"
+
+  //   }
+  // }
+
+
+}
+
---
+++ sunbean/src/models/usuarioModel.js
@@ -0,0 +1,38 @@
+var database = require("../database/config")
+
+function listar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM usuario;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function entrar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function cadastrar(nome, telefone, senha, cpf, email) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, telefone, senha, cpf, email);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO Usuario (nome_rs, telefone, senha, cpf_cnpj, email, fkPlantacao) VALUES ('${nome}',${telefone},'${senha}','${cpf}','${email}','2');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar,
+};
