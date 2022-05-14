9046c9659f3e770a2f04239b24e9a2f44ad67df7
Pedr0Henriqu <pedro.mendonca@sptech.school>
2022-05-14 18:57:48 -0300

    Conectando api nas páginas de login, cadastro de usuario e de plantacao/ inserindo primeira letra do usuario em maiuscula na página de plantacao/ atualizando a modelagem igual a banco de dados na nuvem

--- sunbean/src/models/usuarioModel.js
+++ sunbean/src/models/usuarioModel.js
@@ -36,7 +36,7 @@ function plantacao(nome, hectares, cep, numero) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, hectares, cep, numero);
     
     var instrucao = `
-            insert into plantacao (nome, cep, numero, hectares) values
+            insert into plantacao (nome, cep, numero, qtdHectares) values
             ('${nome}','${cep}','${numero}','${hectares}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
